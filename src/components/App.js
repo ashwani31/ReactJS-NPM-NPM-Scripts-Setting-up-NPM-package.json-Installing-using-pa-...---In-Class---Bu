bc095206307bd9b0ac78e2e883e38961eda7838a
ashwani31 <99326851+ashwani31@users.noreply.github.com>
2022-07-13 18:50:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,29 @@
-import React, { Component, useState } from "react";
+import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [show, setShow] = useState(false);
+  const [show,setShow] = useState(false);
   const showFn = () => {
     setShow(true);
   };
   return (
-    <>
+   
+    <div id="main">
+      // Do not alter the main div
+      <>
       <button id="click" onClick={showFn}>
-        Click
-      </button>
-      {show === true && (
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>
-      )}
+        show
+        </button>
+        {show && (
+          <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy
+          </p>
+        )}
+    
     </>
+
+    </div>
   );
 }
+
 
 export default App;
